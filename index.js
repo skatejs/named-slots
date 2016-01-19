e0e0be0c5afa88775bd2340de6c10d383edec7b1
Trey Shugart <treshugart@gmail.com>
2016-01-19 23:08:01 +1100

    Update. Still need to clean up and export the right functions.

--- index.js
+++ index.js
@@ -1,146 +1,189 @@
 const el = Node.prototype;
-const def = Object.defineProperty.bind(Object);
-
-
-// Notes:
-//
-// - Slots are resolved only when needed.
-// - If a slot is removed, it's not removed from the cache until
-// - This only polyfills part of the API.
+const mapPatch = new WeakMap();
+const mapSlots = new WeakMap();
+const mapSlotsDefault = new WeakMap();
+const prop = Object.defineProperty.bind(Object);
 
 
 // Helpers.
 
-function find (elem, slot) {
-  return elem.querySelector(`[slot-target="${slot}"]`);
+function getSlotName (elem, node) {
+  return node.getAttribute && node.getAttribute('slot') || mapSlotsDefault.get(elem);
 }
 
-function getter (elem, name, get) {
-  def(elem, name, { get });
-}
-
-function slot (elem, newNode) {
-  const name = newNode.getAttribute && newNode.getAttribute('slot') || 'default';
-  
-  // Cache slots on element;
-  if (!elem.__slots) {
-    elem.__slots = {}
-  }
-  
-  // Try to find and cache the specific slot if there is one.
-  if (!elem.__slots[name]) {
-    elem.__slots[name] = find(elem, name) || find(elem, 'default') || find(elem, '');
-  }
-  
-  // Return the slot if it still exists otherwise delete it from the cache.
-  if (elem.__slots[name]) {
-    if (elem.__slots[name].parentNode) {
-      return elem.__slots[name];
-    } else {
-      delete elem.__slots[name];
-    }
-  }
+function nodeToArray (node) {
+  return node instanceof DocumentFragment ? [].slice.call(node.childNodes) : [node];
 }
 
 
 // Prop overrides.
 
-function childElementCount () {
-  return this.children.length;
-}
-
-function childNodes () {
-  const nodes = [];
-  const slots = this.__slots || {};
-  for (var name in slots) {
-    var slot = slots[name];
-    var chNodes = slot.childNodes;
-    var chNodesLen = chNodes.length;
-    for (var a = 0; a < chNodesLen; a++) {
-      nodes.push(chNodes[a]);
+const props = {
+  childElementCount: {
+    get () {
+      return this.children.length;
+    }
+  },
+  childNodes: {
+    get () {
+      return (mapSlots.get(this) || []).reduce((prev, curr) => prev.concat(this[curr]), []);
+    }
+  },
+  children: {
+    get () {
+      return this.childNodes.filter(node => node.nodeType === 1);
+    }
+  },
+  firstChild: {
+    get () {
+      return this.childNodes[0];
+    }
+  },
+  firstElementChild: {
+    get () {
+      return this.children[0];
+    }
+  },
+  innerHTML: {
+    get () {
+      return this.childNodes.map(node => node.outerHTML || node.textContent).join('');
+    },
+    set (val) {
+      this.appendChild(skate.fragment(val));
+    }
+  },
+  lastChild: {
+    get () {
+      const ch = this.childNodes;
+      return ch[ch.length - 1];
+    }
+  },
+  lastElementChild: {
+    get () {
+      const ch = this.children;
+      return ch[ch.length - 1];
+    }
+  },
+  outerHTML: {
+    get () {
+      var name = this.tagName.toLowerCase();
+      var attributes = [].slice.call(this.attributes).map(function (attr) {
+        return ` ${attr.name}${attr.value ? `=${attr.value}` : ''}`;
+      });
+      return `<${name}${attributes}>${this.innerHTML}</${name}>`;
+    }
+  },
+  textContent: {
+    get () {
+      return this.childNodes.map(node => node.textContent).join('');
+    },
+    set (val) {
+      const slot = mapSlotsDefault.get(this);
+      if (slot) {
+        this[slot] = document.createTextNode(val);
+      }
     }
   }
-  return nodes;
-}
-
-function children () {
-  return this.childNodes.filter(node => node.nodeType === 1);
-}
-
-function firstChild () {
-  return this.childNodes[0];
-}
-
-function firstElementChild () {
-  return this.children[0];
-}
-
-function innerHTML () {
-  return this.childNodes.map(node => node.outerHTML || node.textContent).join('');
-}
-
-function lastChild () {
-  const ch = this.childNodes;
-  return ch[ch.length - 1];
-}
-
-function lastElementChild () {
-  const ch = this.children;
-  return ch[ch.length - 1];
-}
-
-function outerHTML () {
-  
-}
-
-function textContent () {
-  return this.childNodes.map(node => node.textContent).join('');
-}
+};
 
 
 // Method overrides.
 
-function appendChild (newNode) {
-  const target = slot(this, newNode);
-  return target && target.appendChild(newNode);
-}
-
-function hasChildNodes () {
-  return this.childNodes.length;
-}
-
-function insertBefore (newNode, refNode) {
-  const target = slot(this, newNode);
-  return target && target.insertBefore(newNode, refNode);
-}
-
-function removeChild (refNode) {
-  const target = slot(this, refNode);
-  return target && target.removeChild(refNode);
-}
-
-function replaceChild (newNode, refNode) {
-  const target = slot(this, refNode);
-  return target && target.replaceChild(newNode, refNode);
-}
+const funcs = {
+  appendChild (newNode) {
+    const name = getSlotName(this, newNode);
+    if (!name && !this[name]) return;
+    this[name] = this[name].concat(nodeToArray(newNode));
+    return newNode;
+  },
+  hasChildNodes () {
+    return this.childNodes.length > 0;
+  },
+  insertBefore (newNode, refNode) {
+    const name = getSlotName(this, newNode);
+    if (!name || !this[name]) return;
+    const index = this[name].indexOf(refNode);
+    this[name] = this[name].slice(0, index).concat(nodeToArray(newNode)).concat(this[name].slice(index));
+    return newNode;
+  },
+  removeChild (refNode) {
+    const name = getSlotName(this, refNode);
+    if (!name && !this[name]) return;
+    const index = this[name].indexOf(refNode);
+    this[name] = this[name].slice(0, index).concat(this[name].slice(index + 1));
+    return refNode;
+  },
+  replaceChild (newNode, refNode) {
+    const name = getSlotName(this, newNode);
+    if (!name || !this[name]) return;
+    const index = this[name].indexOf(refNode);
+    this[name] = this[name].slice(0, index).concat(nodeToArray(newNode)).concat(this[name].slice(index + 1));
+    return refNode;
+  }
+};
 
 
 // Polyfill.
 
-export default function polyfill (node) {
-  getter(node, 'childElementCount', childElementCount);
-  getter(node, 'childNodes', childNodes);
-  getter(node, 'children', children);
-  getter(node, 'firstChild', firstChild);
-  getter(node, 'firstElementChild', firstElementChild);
-  getter(node, 'innerHTML', innerHTML);
-  getter(node, 'innerText', textContent);
-  getter(node, 'lastChild', lastChild);
-  getter(node, 'outerHTML', outerHTML);
-  getter(node, 'textContent', textContent);
-  node.appendChild = appendChild;
-  node.hasChildNodes = hasChildNodes;
-  node.insertBefore = insertBefore;
-  node.removeChild = removeChild;
-  node.replaceChild = replaceChild;
-};
+function polyfill (elem) {
+  if (mapPatch.get(elem)) {
+    return;
+  }
+  
+  for (var name in props) {
+    prop(elem, name, props[name]);
+  }
+  
+  for (var name in funcs) {
+    elem[name] = funcs[name];
+  }
+  
+  mapPatch.set(elem, true);
+}
+
+function slot (opts) {
+  if (!opts) {
+    opts = {
+      default: false,
+      set: null
+    };
+  }
+  
+  return {
+    // Makes sure that whatever is passed in is an array.
+    coerce: function (val) { 
+      return Array.isArray(val) ? val : [val];
+    },
+    
+    // Registers the slot so we can check later.
+    created: function (elem, data) {
+      var slots = mapSlots.get(elem);
+      
+      if (!slots) {
+        mapSlots.set(elem, slots = []);
+      }
+      
+      slots.push(data.name);
+      
+      if (opts.default) {
+        mapSlotsDefault.set(elem, data.name);
+      }
+    },
+    
+    // If an empty value is passed in, ensure that it's an array.
+    'default': function () { 
+      return [];
+    },
+    
+    // Return any initial nodes that match the slot.
+    initial: function (elem, data) {
+      return [].slice.call(elem.childNodes).filter(function (ch) {
+        var slot = (ch.getAttribute && ch.getAttribute('slot')) || (opts.default && data.name);
+        return slot && slot === data.name;
+      });
+    },
+    
+    // User-defined setter.
+    set: opts.set
+  }
+}

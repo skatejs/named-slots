01f714f408e1af8403ebf2e13d81d700f83eb659
Joshua Nelson <jonelson@atlassian.com>
2016-01-25 11:32:25 +1100

    Add skate build for named slots

--- src/index.js
+++ src/index.js
@@ -125,56 +125,56 @@ const funcs = {
 
 // Polyfill.
 
-function polyfill (elem) {
+export function polyfill (elem) {
   if (mapPatch.get(elem)) {
     return;
   }
-  
+
   for (var name in props) {
     prop(elem, name, props[name]);
   }
-  
+
   for (var name in funcs) {
     elem[name] = funcs[name];
   }
-  
+
   mapPatch.set(elem, true);
 }
 
-function slot (opts) {
+export function slot (opts) {
   if (!opts) {
     opts = {
       default: false,
       set: null
     };
   }
-  
+
   return {
     // Makes sure that whatever is passed in is an array.
-    coerce: function (val) { 
+    coerce: function (val) {
       return Array.isArray(val) ? val : [val];
     },
-    
+
     // Registers the slot so we can check later.
     created: function (elem, data) {
       var slots = mapSlots.get(elem);
-      
+
       if (!slots) {
         mapSlots.set(elem, slots = []);
       }
-      
+
       slots.push(data.name);
-      
+
       if (opts.default) {
         mapSlotsDefault.set(elem, data.name);
       }
     },
-    
+
     // If an empty value is passed in, ensure that it's an array.
-    'default': function () { 
+    'default': function () {
       return [];
     },
-    
+
     // Return any initial nodes that match the slot.
     initial: function (elem, data) {
       return [].slice.call(elem.childNodes).filter(function (ch) {
@@ -182,8 +182,9 @@ function slot (opts) {
         return slot && slot === data.name;
       });
     },
-    
+
     // User-defined setter.
     set: opts.set
   }
 }
+

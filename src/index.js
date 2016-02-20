c8798e8c3dd778d7b3517bf4e27451f010cfe634
Trey Shugart <treshugart@gmail.com>
2016-02-20 14:45:27 +1100

    Remove slot module as it's not necessary anymore.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import polyfill from './polyfill';
 import polyfilled from './polyfilled';
 import render from './render';
-import slot from './slot';
 import version from './version';
 
 export default polyfill;
 export {
   polyfilled,
   render,
-  slot,
   version
 };

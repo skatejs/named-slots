fb8a7aa9bf000dc206e5ec9eb129ce0214a416a2
Trey Shugart <treshugart@gmail.com>
2016-03-04 23:01:45 +1100

    Update build.

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.skatejs-named-slots = factory());
+  (global.skatejsNamedSlots = factory());
 }(this, function () {
 
   var mapPolyfilled = new WeakMap();

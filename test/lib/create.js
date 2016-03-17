5e3df7f29e60d400c469adc00f30371523c05ca9
Trey Shugart <treshugart@gmail.com>
2016-03-17 16:36:11 +1100

    Modularise the element create() method for tests.

---
+++ test/lib/create.js
@@ -0,0 +1,9 @@
+export default function (name, attrs = {}, chren = []) {
+  const elem = document.createElement(name);
+  const attrsIsChren = Array.isArray(attrs);
+  chren = attrsIsChren ? attrs : chren;
+  attrs = attrsIsChren ? {} : attrs;
+  Object.keys(attrs).forEach(key => elem.setAttribute(key, attrs[key]));
+  chren.forEach(ch => elem.appendChild(ch));
+  return elem;
+}

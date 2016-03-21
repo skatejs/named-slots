e6d2f48cc7ae310ea0422de6fc4e1e7b2e78a17e
Trey Shugart <treshugart@gmail.com>
2016-03-21 18:29:53 +1100

    Organisation.

--- src/slot/content.js
+++ src/slot/content.js
@@ -1,9 +1,5 @@
 import { assignedNodes, changeListeners, fallbackNodes, fallbackState, debouncedTriggerSlotChangeEvent } from './data';
 
-function get (slot) {
-  return assignedNodes.get(slot);
-}
-
 function removeFromSlot (frag, slot) {
   frag.childNodes.forEach(ch => slot.__removeChild(ch));
 }
@@ -16,47 +12,19 @@ function shouldAffectSlot (slot) {
   return !fallbackState.get(slot);
 }
 
-function triggerSideEffects (slot) {
-  toggle(slot);
-  triggerSlotChangeEvent(slot);
-}
-
-export function appendChild (slot, newNode) {
-  get(slot).appendChild(newNode);
-  shouldAffectSlot(slot) && slot.__appendChild(newNode);
-  triggerSideEffects(slot);
-}
-
-export function insertBefore (slot, newNode, refNode) {
-  get(slot).insertBefore(newNode, refNode);
-  shouldAffectSlot(slot) && slot.__insertBefore(newNode, refNode);
-  triggerSideEffects(slot);
-}
-
-export function removeChild (slot, refNode) {
-  get(slot).removeChild(refNode);
-  shouldAffectSlot(slot) && slot.__removeChild(refNode);
-  triggerSideEffects(slot);
-}
-
-export function replaceChild (slot, newNode, refNode) {
-  get(slot).replaceChild(newNode, refNode);
-  shouldAffectSlot(slot) && slot.__replaceChild(newNode, refNode);
-  triggerSideEffects(slot);
-}
-
-export function toggle (slot) {
-  const aNodes = assignedNodes.get(slot);
-  const fNodes = fallbackNodes.get(slot);
-
+function toggle (slot) {
   if (fallbackState.get(slot)) {
+    const aNodes = assignedNodes.get(slot);
     if (aNodes.hasChildNodes()) {
+      const fNodes = fallbackNodes.get(slot);
       removeFromSlot(fNodes, slot);
       moveIntoSlot(aNodes, slot);
       fallbackState.set(slot, false);
     }
   } else {
+    const aNodes = assignedNodes.get(slot);
     if (!aNodes.hasChildNodes()) {
+      const fNodes = fallbackNodes.get(slot);
       removeFromSlot(aNodes, slot);
       moveIntoSlot(fNodes, slot);
       fallbackState.set(slot, true);
@@ -64,8 +32,37 @@ export function toggle (slot) {
   }
 }
 
-export function triggerSlotChangeEvent (slot) {
+function triggerEvent (slot) {
   if (changeListeners.get(slot)) {
     debouncedTriggerSlotChangeEvent.get(slot)(slot);
   }
+}
+
+function triggerSideEffects (slot) {
+  toggle(slot);
+  triggerEvent(slot);
+}
+
+export function appendChild (slot, newNode) {
+  assignedNodes.get(slot).appendChild(newNode);
+  shouldAffectSlot(slot) && slot.__appendChild(newNode);
+  triggerSideEffects(slot);
+}
+
+export function insertBefore (slot, newNode, refNode) {
+  assignedNodes.get(slot).insertBefore(newNode, refNode);
+  shouldAffectSlot(slot) && slot.__insertBefore(newNode, refNode);
+  triggerSideEffects(slot);
+}
+
+export function removeChild (slot, refNode) {
+  assignedNodes.get(slot).removeChild(refNode);
+  shouldAffectSlot(slot) && slot.__removeChild(refNode);
+  triggerSideEffects(slot);
+}
+
+export function replaceChild (slot, newNode, refNode) {
+  assignedNodes.get(slot).replaceChild(newNode, refNode);
+  shouldAffectSlot(slot) && slot.__replaceChild(newNode, refNode);
+  triggerSideEffects(slot);
 }

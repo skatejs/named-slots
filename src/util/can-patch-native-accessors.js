a768ffda863658c0eaecef9eec3529b124205e59
Trey Shugart <treshugart@gmail.com>
2016-03-31 14:43:09 +1100

    Prefix global var access with "window".

--- src/util/can-patch-native-accessors.js
+++ src/util/can-patch-native-accessors.js
@@ -1,3 +1,3 @@
 // Any code referring to this is because it has to work around this bug in
 // WebKit: https://bugs.webkit.org/show_bug.cgi?id=49739
-export default !!Object.getOwnPropertyDescriptor(Node.prototype, 'parentNode').get;
+export default !!Object.getOwnPropertyDescriptor(window.Node.prototype, 'parentNode').get;

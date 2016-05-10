fb6f9c6714cd2204a76b69450b8985ec09985fcd
Trey Shugart <treshugart@gmail.com>
2016-05-10 22:57:41 +1000

    Code reuse.

--- src/util/can-patch-native-accessors.js
+++ src/util/can-patch-native-accessors.js
@@ -1,3 +1,8 @@
 // Any code referring to this is because it has to work around this bug in
 // WebKit: https://bugs.webkit.org/show_bug.cgi?id=49739
-export default !!(Object.getOwnPropertyDescriptor(window.Node.prototype, 'parentNode') && Object.getOwnPropertyDescriptor(window.Node.prototype, 'parentNode').get);
+
+import getPropertyDescriptor from './get-property-descriptor';
+
+const nativeParentNode = getPropertyDescriptor(Node.prototype, 'parentNode');
+
+export default !!nativeParentNode;

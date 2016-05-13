6575b61d200bf6fe4affef2cb3343f6e2d37268e
Joscha Feth <jfeth@atlassian.com>
2016-05-13 15:25:36 +1000

    chore: new build

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,15 @@
     (global.skatejsNamedSlots = factory());
 }(this, function () {
 
+    var babelHelpers = {};
+    babelHelpers.typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+      return typeof obj;
+    } : function (obj) {
+      return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj;
+    };
+    babelHelpers;
+
+
     function __commonjs(fn, module) { return module = { exports: {} }, fn(module, module.exports), module.exports; }
 
     function eachChildNode(node, func) {
@@ -36,7 +45,7 @@
     var div = document.createElement('div');
 
     function getPrototype(obj, key) {
-      var descriptor = void 0;
+      var descriptor = undefined;
 
       while (obj && !(descriptor = Object.getOwnPropertyDescriptor(obj, key))) {
         obj = Object.getPrototypeOf(obj);
@@ -163,56 +172,290 @@
 
     var version = '0.0.1';
 
-    var WeakMap = window.WeakMap || function () {
-      var index = 0;
-      function Wm() {
-        this.key = '____weak_map_' + index++;
+    var weakmap = __commonjs(function (module, exports) {
+    /* (The MIT License)
+     *
+     * Copyright (c) 2012 Brandon Benvie <http://bbenvie.com>
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+     * associated documentation files (the 'Software'), to deal in the Software without restriction,
+     * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+     * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+     * furnished to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included with all copies or
+     * substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+     * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+     * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  CLAIM,
+     * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+     */
+
+    // Original WeakMap implementation by Gozala @ https://gist.github.com/1269991
+    // Updated and bugfixed by Raynos @ https://gist.github.com/1638059
+    // Expanded by Benvie @ https://github.com/Benvie/harmony-collections
+
+    void function (global, undefined_, undefined) {
+      var getProps = Object.getOwnPropertyNames,
+          defProp = Object.defineProperty,
+          toSource = Function.prototype.toString,
+          create = Object.create,
+          hasOwn = Object.prototype.hasOwnProperty,
+          funcName = /^\n?function\s?(\w*)?_?\(/;
+
+      function define(object, key, value) {
+        if (typeof key === 'function') {
+          value = key;
+          key = nameOf(value).replace(/_$/, '');
+        }
+        return defProp(object, key, { configurable: true, writable: true, value: value });
       }
-      Wm.prototype = {
-        delete: function _delete(obj) {
-          if (obj) {
-            delete obj[this.key];
+
+      function nameOf(func) {
+        return typeof func !== 'function' ? '' : 'name' in func ? func.name : toSource.call(func).match(funcName)[1];
+      }
+
+      // ############
+      // ### Data ###
+      // ############
+
+      var Data = function () {
+        var dataDesc = { value: { writable: true, value: undefined } },
+            datalock = 'return function(k){if(k===s)return l}',
+            uids = create(null),
+            createUID = function createUID() {
+          var key = Math.random().toString(36).slice(2);
+          return key in uids ? createUID() : uids[key] = key;
+        },
+            globalID = createUID(),
+            storage = function storage(obj) {
+          if (hasOwn.call(obj, globalID)) return obj[globalID];
+
+          if (!Object.isExtensible(obj)) throw new TypeError("Object must be extensible");
+
+          var store = create(null);
+          defProp(obj, globalID, { value: store });
+          return store;
+        };
+
+        // common per-object storage area made visible by patching getOwnPropertyNames'
+        define(Object, function getOwnPropertyNames(obj) {
+          var props = getProps(obj);
+          if (hasOwn.call(obj, globalID)) props.splice(props.indexOf(globalID), 1);
+          return props;
+        });
+
+        function Data() {
+          var puid = createUID(),
+              secret = {};
+
+          this.unlock = function (obj) {
+            var store = storage(obj);
+            if (hasOwn.call(store, puid)) return store[puid](secret);
+
+            var data = create(null, dataDesc);
+            defProp(store, puid, {
+              value: new Function('s', 'l', datalock)(secret, data)
+            });
+            return data;
+          };
+        }
+
+        define(Data.prototype, function get(o) {
+          return this.unlock(o).value;
+        });
+        define(Data.prototype, function set(o, v) {
+          this.unlock(o).value = v;
+        });
+
+        return Data;
+      }();
+
+      var WM = function (data) {
+        var validate = function validate(key) {
+          if (key == null || (typeof key === 'undefined' ? 'undefined' : babelHelpers.typeof(key)) !== 'object' && typeof key !== 'function') throw new TypeError("Invalid WeakMap key");
+        };
+
+        var wrap = function wrap(collection, value) {
+          var store = data.unlock(collection);
+          if (store.value) throw new TypeError("Object is already a WeakMap");
+          store.value = value;
+        };
+
+        var unwrap = function unwrap(collection) {
+          var storage = data.unlock(collection).value;
+          if (!storage) throw new TypeError("WeakMap is not generic");
+          return storage;
+        };
+
+        var initialize = function initialize(weakmap, iterable) {
+          if (iterable !== null && (typeof iterable === 'undefined' ? 'undefined' : babelHelpers.typeof(iterable)) === 'object' && typeof iterable.forEach === 'function') {
+            iterable.forEach(function (item, i) {
+              if (item instanceof Array && item.length === 2) set.call(weakmap, iterable[i][0], iterable[i][1]);
+            });
           }
-        },
-        get: function get(obj) {
-          return obj ? obj[this.key] : null;
-        },
-        has: function has(obj) {
-          return obj ? typeof obj[this.key] !== 'undefined' : false;
-        },
-        set: function set(obj, val) {
-          if (obj) {
-            var key = this.key;
-            if (typeof obj[key] === 'undefined') {
-              Object.defineProperty(obj, key, {
-                configurable: true,
-                enumerable: false,
-                writable: true,
-                value: val
-              });
-            } else {
-              obj[key] = val;
+        };
+
+        function WeakMap(iterable) {
+          if (this === global || this == null || this === WeakMap.prototype) return new WeakMap(iterable);
+
+          wrap(this, new Data());
+          initialize(this, iterable);
+        }
+
+        function get(key) {
+          validate(key);
+          var value = unwrap(this).get(key);
+          return value === undefined_ ? undefined : value;
+        }
+
+        function set(key, value) {
+          validate(key);
+          // store a token for explicit undefined so that "has" works correctly
+          unwrap(this).set(key, value === undefined ? undefined_ : value);
+        }
+
+        function has(key) {
+          validate(key);
+          return unwrap(this).get(key) !== undefined;
+        }
+
+        function delete_(key) {
+          validate(key);
+          var data = unwrap(this),
+              had = data.get(key) !== undefined;
+          data.set(key, undefined);
+          return had;
+        }
+
+        function toString() {
+          unwrap(this);
+          return '[object WeakMap]';
+        }
+
+        try {
+          var src = ('return ' + delete_).replace('e_', '\\u0065'),
+              del = new Function('unwrap', 'validate', src)(unwrap, validate);
+        } catch (e) {
+          var del = delete_;
+        }
+
+        var src = ('' + Object).split('Object');
+        var stringifier = function toString() {
+          return src[0] + nameOf(this) + src[1];
+        };
+
+        define(stringifier, stringifier);
+
+        var prep = { __proto__: [] } instanceof Array ? function (f) {
+          f.__proto__ = stringifier;
+        } : function (f) {
+          define(f, stringifier);
+        };
+
+        prep(WeakMap);
+
+        [toString, get, set, has, del].forEach(function (method) {
+          define(WeakMap.prototype, method);
+          prep(method);
+        });
+
+        return WeakMap;
+      }(new Data());
+
+      var defaultCreator = Object.create ? function () {
+        return Object.create(null);
+      } : function () {
+        return {};
+      };
+
+      function createStorage(creator) {
+        var weakmap = new WM();
+        creator || (creator = defaultCreator);
+
+        function storage(object, value) {
+          if (value || arguments.length === 2) {
+            weakmap.set(object, value);
+          } else {
+            value = weakmap.get(object);
+            if (value === undefined) {
+              value = creator(object);
+              weakmap.set(object, value);
             }
           }
+          return value;
         }
-      };
-      return Wm;
-    }();
 
-    // polyfilling CustomEvent for the IE
-    (function () {
-      if (typeof window.CustomEvent === 'function') return false;
-
-      function CustomEvent(event, params) {
-        params = params || { bubbles: false, cancelable: false, detail: undefined };
-        var evt = document.createEvent('CustomEvent');
-        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
-        return evt;
+        return storage;
       }
 
-      CustomEvent.prototype = window.Event.prototype;
-      window.CustomEvent = CustomEvent;
-    })();
+      if (typeof module !== 'undefined') {
+        module.exports = WM;
+      } else if (typeof exports !== 'undefined') {
+        exports.WeakMap = WM;
+      } else if (!('WeakMap' in global)) {
+        global.WeakMap = WM;
+      }
+
+      WM.createStorage = createStorage;
+      if (global.WeakMap) global.WeakMap.createStorage = createStorage;
+    }((0, eval)('this'));
+    });
+
+    var WeakMap = (weakmap && typeof weakmap === 'object' && 'default' in weakmap ? weakmap['default'] : weakmap);
+
+    // Polyfill for creating CustomEvents on IE9/10/11
+
+    // code pulled from:
+    // https://github.com/d4tocchini/customevent-polyfill
+    // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+    try {
+        var ce = new window.CustomEvent('test', {
+            bubbles: false,
+            cancelable: true,
+            detail: {
+                x: 'y'
+            }
+        });
+        ce.preventDefault();
+        if (ce.defaultPrevented !== true) {
+            // IE has problems with .preventDefault() on custom events
+            // http://stackoverflow.com/questions/23349191
+            throw new Error('Could not prevent default');
+        }
+    } catch (e) {
+        var CustomEvent$1 = function CustomEvent(event, params) {
+            var evt;
+            params = params || {
+                bubbles: false,
+                cancelable: false,
+                detail: undefined
+            };
+
+            evt = document.createEvent("CustomEvent");
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            var origPrevent = evt.preventDefault;
+            evt.preventDefault = function () {
+                origPrevent.call(this);
+                try {
+                    Object.defineProperty(this, 'defaultPrevented', {
+                        get: function get() {
+                            return true;
+                        }
+                    });
+                } catch (e) {
+                    this.defaultPrevented = true;
+                }
+            };
+            return evt;
+        };
+
+        CustomEvent$1.prototype = window.Event.prototype;
+        window.CustomEvent = CustomEvent$1; // expose definition to window
+    }
 
     var arrProto = Array.prototype;
     var forEach = arrProto.forEach;
@@ -859,7 +1102,7 @@
       nextElementSibling: {
         get: function get() {
           var host = this;
-          var found = void 0;
+          var found = undefined;
           return eachChildNode(this.parentNode, function (child) {
             if (found && child.nodeType === 1) {
               return child;
@@ -904,7 +1147,7 @@
       previousElementSibling: {
         get: function get() {
           var host = this;
-          var found = void 0;
+          var found = undefined;
           return eachChildNode(this.parentNode, function (child) {
             if (found && host === child) {
               return found;

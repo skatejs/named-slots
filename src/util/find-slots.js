c5ede5962c44b1518919832d851441181e50d201
Trey Shugart <treshugart@gmail.com>
2016-06-22 16:24:48 +1000

    docs(slotting): Add comment describing why so many arguments are being passed to createTreeWalker().

--- src/util/find-slots.js
+++ src/util/find-slots.js
@@ -2,6 +2,7 @@ import isRootNode from './is-root-node';
 import isSlotNode from './is-slot-node';
 
 export default function(root) {
+  // IE requies the last argument, so we must provide both the 3rd and last.
   const walker = document.createTreeWalker(root, Node.SHOW_ELEMENT, null, false);
   const slots = [];
 

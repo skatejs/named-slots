20236a9cada3fcdd5241ff55f0258a7ef52f1292
Trey Shugart <treshugart@gmail.com>
2016-06-22 16:33:44 +1000

    fix(ie): Fix createTreeWalker() quirsk in IE.

--- src/util/find-slots.js
+++ src/util/find-slots.js
@@ -2,10 +2,15 @@ import isRootNode from './is-root-node';
 import isSlotNode from './is-slot-node';
 
 export default function(root) {
-  // IE requies the last argument, so we must provide both the 3rd and last.
-  const walker = document.createTreeWalker(root, Node.SHOW_ELEMENT, null, false);
   const slots = [];
 
+  // IE complains if you call nextNode() on a tree walker that isn't an element.
+  if (root.nodeType !== Node.ELEMENT_NODE) {
+    return [];
+  }
+
+  // IE requies the last argument, so we must provide both the 3rd and last.
+  const walker = document.createTreeWalker(root, Node.SHOW_ELEMENT, null, false);
   let inDescendantRoot = false;
 
   while (walker.nextNode()) {

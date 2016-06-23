868657feabd383fba65491f81a1246cb8607376f
Nadia Makarevich <nmakarevich@atlassian.com>
2016-06-23 10:32:30 +1000

    test: added some tests to break find-slots

--- test/unit/util/find-slots.js
+++ test/unit/util/find-slots.js
@@ -15,7 +15,7 @@ describe('util/find-slots called from', () => {
     const host = document.createElement('div');
     const slot = document.createElement('slot');
 
-    host.attachShadow({ mode: 'open' });
+    host.attachShadow({mode: 'open'});
     host.shadowRoot.appendChild(slot);
 
     expect(findSlots(host).length).to.equal(0);
@@ -25,10 +25,43 @@ describe('util/find-slots called from', () => {
     const host = document.createElement('div');
     const slot = document.createElement('slot');
 
-    host.attachShadow({ mode: 'open' });
+    host.attachShadow({mode: 'open'});
     host.shadowRoot.appendChild(slot);
 
     expect(findSlots(host.shadowRoot).length).to.equal(1);
     expect(findSlots(host.shadowRoot)[0]).to.equal(slot);
   });
+
+  it('different combinations of slots, roots and nodes', function () {
+    const host = document.createElement('div');
+    host.innerHTML = '<div></div><div></div><div></div>';
+    expect(findSlots(host).length).to.equal(0);
+
+    let root1 = host.childNodes[1].attachShadow({mode: 'open'});
+    root1.innerHTML = '<div></div><slot></slot><div></div>';
+    expect(findSlots(host).length).to.equal(0);
+    expect(findSlots(root1).length).to.equal(1);
+    expect(findSlots(root1)[0].name).to.equal(null);
+
+    root1.innerHTML = '<div></div><slot></slot><div><slot name="name1"></slot></div><slot name="name2"></slot>';
+    expect(findSlots(host).length).to.equal(0);
+    expect(findSlots(root1).length).to.equal(3);
+    expect(findSlots(root1)[0].name).to.equal(null);
+    expect(findSlots(root1)[1].name).to.equal('name1');
+    expect(findSlots(root1)[2].name).to.equal('name2');
+
+    let root2 = root1.childNodes[2].attachShadow({mode: 'open'});
+    root2.innerHTML = '<div></div><slot></slot><div><slot name="name3"></slot></div><slot name="name4"></slot>';
+
+    expect(findSlots(host).length).to.equal(0);
+    expect(findSlots(root1).length).to.equal(3);
+    expect(findSlots(root1)[0].name).to.equal(null);
+    expect(findSlots(root1)[1].name).to.equal('name1');
+    expect(findSlots(root1)[2].name).to.equal('name2');
+
+    expect(findSlots(root2).length).to.equal(3);
+    expect(findSlots(root2)[0].name).to.equal(null);
+    expect(findSlots(root2)[1].name).to.equal('name3');
+    expect(findSlots(root2)[2].name).to.equal('name4');
+  })
 });

c6ccecc3a1384af9f1b3a696324e10288b220b11
Nadia Makarevich <nmakarevich@atlassian.com>
2016-07-26 16:32:58 +1000

    feat: added native test

--- test/perf.js
+++ test/perf.js
@@ -11,11 +11,26 @@ describe('add / remove', () => {
     el.removeChild(ch);
   }
 
+  function fn_native(elem) {
+    const ch = div();
+    const el = elem;
+    el.__appendChild(ch);
+    el.__removeChild(ch);
+  }
+
+
+  benchmark(() => {
+    const elem = div();
+    fn_native(elem);
+  }).then((opsPerSec) => {
+    console.log('native: ', opsPerSec);
+  });
+
   benchmark(() => {
     const elem = div();
     fn(elem);
   }).then((opsPerSec) => {
-    console.log('native: ', opsPerSec);
+    console.log('polyfilled (no root): ', opsPerSec);
   });
 
   benchmark(() => {
@@ -23,7 +38,7 @@ describe('add / remove', () => {
     elem.attachShadow({ mode: 'closed' });
     fn(elem);
   }).then((opsPerSec) => {
-    console.log('prollyfill (no slot): ', opsPerSec);
+    console.log('prolyfill (no slot): ', opsPerSec);
   });
 
   benchmark(() => {
@@ -32,6 +47,6 @@ describe('add / remove', () => {
     root.innerHTML = '<slot></slot>';
     fn(elem);
   }).then((opsPerSec) => {
-    console.log('prollyfill (default slot): ', opsPerSec);
+    console.log('prolyfill (default slot): ', opsPerSec);
   });
 });

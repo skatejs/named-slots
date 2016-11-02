028aace1b54cdee4b1f7e8d046e22d35a6edac25
Trey Shugart <treshugart@gmail.com>
2016-11-02 13:45:12 +1100

    refactor: Refactor the touched test file to use the create() test helper.
    #137

--- test/unit/dom/DocumentFragment.js
+++ test/unit/dom/DocumentFragment.js
@@ -5,20 +5,20 @@ import create from '../../lib/create';
 describe('DocumentFragment', () => {
   it('should report the correct parent node for nested nodes', () => {
     const frag = document.createDocumentFragment();
-    const elem = document.createElement('div');
+    const elem = create();
     frag.appendChild(elem);
     expect(elem.parentNode).to.equal(frag);
   });
 
   it('should remove child elements when appending a document fragment', () => {
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
+    const div1 = create();
+    const div2 = create();
 
     const frag = document.createDocumentFragment();
     frag.appendChild(div1);
     frag.appendChild(div2);
 
-    const elem = document.createElement('div');
+    const elem = create();
     elem.appendChild(frag);
     expect(frag.childNodes.length).to.equal(0);
   });

6615abaca6aba32a46f0b7df9ea8470cd7172926
Trey Shugart <treshugart@gmail.com>
2016-11-02 13:43:23 +1100

    fix: Fix document fragment appending issue.
    #137

--- src/v1/index.js
+++ src/v1/index.js
@@ -422,10 +422,10 @@ function appendChildOrInsertBefore (host, newNode, refNode) {
   switch (nodeType) {
     case 'fragment':
     case 'node':
-      if (canPatchNativeAccessors) {
-        nodeToParentNodeMap.set(newNode, host);
-        return host.__insertBefore(newNode, refNode !== undefined ? refNode : null);
-      }
+      // if (canPatchNativeAccessors) {
+      //   nodeToParentNodeMap.set(newNode, host);
+      //   return host.__insertBefore(newNode, refNode !== undefined ? refNode : null);
+      // }
       return addNodeToNode(host, newNode, refNode);
     case 'slot':
       return addNodeToSlot(host, newNode, refNode);
@@ -695,7 +695,6 @@ const members = {
   insertBefore: {
     value (newNode, refNode) {
       appendChildOrInsertBefore(this, newNode, refNode);
-
       return newNode;
     }
   },
